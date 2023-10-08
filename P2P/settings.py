28f3a2cbb833d306344fe26e259264d1739c9746
Your Name <you@example.com>
2023-10-08 14:57:09 +0600

    no psql

--- P2P/settings.py
+++ P2P/settings.py
@@ -119,11 +119,11 @@ WSGI_APPLICATION = 'P2P.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.environ.get('POSTGRES_DB'),
-        'USER': os.environ.get('POSTGRES_USER'),
-        'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
-        'HOST': os.environ.get('POSTGRES_HOST'),
-        'PORT': os.environ.get('POSTGRES_PORT'),
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'db',
+        'PORT': 5432
     }
 }
 
