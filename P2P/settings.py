917907e84bec12ed1430519e4f03cdf197ad39d3
Your Name <you@example.com>
2023-10-14 20:47:40 +0600

    asd

--- P2P/settings.py
+++ P2P/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config('DEBUG', default=False, cast=bool)
+DEBUG = True
 
 ALLOWED_HOSTS = ['localhost', '84.201.184.40', '127.0.0.1', 'arbitcash.space', '54.82.6.149']
 
